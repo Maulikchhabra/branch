495238d5693dd40a220160abc80db54b248d8dbe
maulik <maulikchhabra@gmail.com>
2019-03-07 10:24:50 +0530

    Added comments to the program

--- f1.cpp
+++ f1.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
+#include<iostream>//header file//
 using namespace std;
 class branch
 {
         public:
-                void display()
+                void display()//function//
                 {
                         cout<<"Hello there";
                         cout<<"How are you?";
@@ -13,6 +13,7 @@ int main()
 {
         branch b1;
 	b1.display();
+
  return 0;
 }
 
