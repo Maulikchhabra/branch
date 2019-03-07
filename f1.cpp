e2e7437313a7db0fa269e1551106a26b3bbac5e7
maulik <maulikchhabra@gmail.com>
2019-03-07 10:23:34 +0530

    Learning branching

---
+++ f1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class branch
+{
+        public:
+                void display()
+                {
+                        cout<<"Hello there";
+                        cout<<"How are you?";
+                }
+};
+int main()
+{
+        branch b1;
+	b1.display();
+ return 0;
+}
+
+
