b26bdb97c20626957a8f809da9ce028698a27d6e
maulik <maulikchhabra@gmail.com>
2019-03-07 10:35:11 +0530

    Added a new add function

--- f1.cpp
+++ f1.cpp
@@ -8,11 +8,19 @@ class branch
                         cout<<"Hello there";
                         cout<<"How are you?";
                 }
+		void add()
+		{
+			int x=10,y=20,sum;
+			sum =x+y;
+			return sum;
+		}
+
 };
 int main()
 {
         branch b1;
 	b1.display();
+	b1.add();
 
  return 0;
 }
