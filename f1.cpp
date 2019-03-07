35d9ac3c4b7acf7d4f529810fe77f80fbc6d2d9d
maulik <maulikchhabra@gmail.com>
2019-03-07 10:29:13 +0530

    Added new function show in the program

--- f1.cpp
+++ f1.cpp
@@ -8,11 +8,18 @@ class branch
                         cout<<"Hello there";
                         cout<<"How are you?";
                 }
+		void show()
+		{
+			cout<<"This is a practise program";
+		}
+
+			
 };
 int main()
 {
         branch b1;
 	b1.display();
+	b1.show();
 
  return 0;
 }
